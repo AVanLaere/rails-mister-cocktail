1600ceee1c7d63475428691f938d97ef6317726b
Andy Van Laere <andy@homesweetoregon.com>
2020-04-09 12:44:46 +0300

    added dose model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_093514) do
+ActiveRecord::Schema.define(version: 2020_04_09_094402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 2020_04_09_093514) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.bigint "cocktail_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+  end
+
+  add_foreign_key "doses", "cocktails"
 end
